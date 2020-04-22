014b7c4a14f2dcf3655bd4b9f8e3600e8560a76e
shdfe <56097891+shdfe@users.noreply.github.com>
2020-04-21 21:02:48 -0700

    Update tweetbot.py

--- tweetbot.py
+++ tweetbot.py
@@ -1,7 +1,5 @@
 import tweepy
-from selenium import webdriver
 import requests
-from requests_oauthlib import OAuth1Session
 auth = tweepy.OAuthHandler('TFQ8j7iucP5OBpFl4RrxBN5h8', 'AlKCktwXKFyBHlMt4ZqRSPKpJ6YDapm79d1wyAI5TbJ4PEt1ue')
 auth.set_access_token('1233849629256503296-8id8G71A9RcfqrAurLDQiIX1RNsYEW', 'rIkKFwmrL4a331835GGiDYMtGnOrhZE2zBub3qHqCM1BF')
 api = tweepy.API(auth)
@@ -16,10 +14,3 @@ if res.status_code == requests.codes.ok:
 else:
     pass
  
-
-tweets = api.user_timeline(screen_name='BarackObama')
-first_tweet = tweets[0]
-print(first_tweet.text)
-
-for follower in tweepy.Cursor(api.followers).items():
-    follower.follow()
